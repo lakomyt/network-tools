1c262968a8bece0cc6664e59b0decc7b5cc403df
tykazzz <tlakomy2001@gmail.com>
2019-09-24 19:12:24 +0200

    remove localhost from scan, add updates

--- scan.sh
+++ scan.sh
@@ -5,14 +5,32 @@
 #syntax: scan /dir target
 
 SCAN_DIR=$1
+TARGET=$2
+
 cd $SCAN_DIR
 
-echo Scanning with nmap...
-nmap -sn $2 | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > hosts.nmap
-echo Scanning with arp-scan...
-arp-scan -l | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > hosts.arp
-echo
-sort -n hosts.nmap hosts.arp | uniq | sort -n | tee hosts.lst
-rm hosts.nmap hosts.arp
+scan(){
+	echo Scanning with nmap...
+	nmap -sn $TARGET | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > hosts.nmap
+	echo Scanning with arp-scan...
+	arp-scan -l | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > hosts.arp
+	echo DONE.
+	sort -n hosts.nmap hosts.arp | uniq | sort -n | tee $1
+	rm hosts.nmap hosts.arp
+	cat $1 | grep "`ip route get 1.1.1.1 | head -1 | awk '{print $7}'`" -v > $1
+}
+
+scan hosts.lst
+
+while [ 1 == 1 ]; do
+	sleep 3600
+	scan NEW_hosts.lst
+	diff hosts.lst NEW_hosts.lst | grep ">" > /dev/null
+	if [ $? -eq 0 ]; then
+		date +%H:%M_%e-%m-%Y >> hosts.lst
+		diff hosts.lst NEW_hosts.lst | grep ">" | sed 's/> //' >> hosts.lst
+		rm NEW_hosts.lst
+	fi
+done
 
 exit
